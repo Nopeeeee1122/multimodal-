42245102fa95a595659519934c4419dc7ce67c7d
Nopeeeee1122 <71562339+Nopeeeee1122@users.noreply.github.com>
2021-11-20 23:50:58 +0800

    Add files via upload

---
+++ src/utils/utils.py
@@ -0,0 +1,11 @@
+import torch
+from torch.autograd import Variable, Function
+
+def to_np(x):
+    return x.data.cpu().numpy()
+
+def to_var(x):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x)
+
